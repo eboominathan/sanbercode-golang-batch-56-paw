54ff910783b0a3d2b6b4d0258281f278a53897df
Pudyasto Adi W <mr.pudyasto@gmail.com>
2024-05-24 21:57:34 +0700

    Update main.go

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 
   	// log.Fatal(http.ListenAndServe(appPort, handler))
 	
-	router.Run("0.0.0.0:" + envPortOr("3000"))
+	router.Run(":" + envPortOr("3000"))
 }
 
 func envPortOr(port string) string {
